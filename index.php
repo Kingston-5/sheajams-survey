ed092be065037b58d5454d2b0ab342da86d6051c
Kingston-5 <67066977+Kingston-5@users.noreply.github.com>
2022-01-16 23:21:47 +0200

    mismatched start btn - temp fix

--- index.php
+++ index.php
@@ -66,11 +66,12 @@
         <form action="#" method="post">
             <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
             <?php
-            if (Cookie::get('total_question') == 12) {
-                echo '<button class="btn">Thank you for taking our survey</button>';
-            } else {
-                echo '<input class="btn" type="submit" value="Take Survey">';
-            } ?>
+//             if (Cookie::get('total_question') == 12) {
+//                 echo '<button class="btn">Thank you for taking our survey</button>';
+//             } else {
+//                 echo '<input class="btn" type="submit" value="Take Survey">';
+//             } ?>
+            <input class="btn" type="submit" value="Take Survey">
         </form>
     </div> 
     
