f3918650541056563a8b5b5e938d318baedde503
Kingston-5 <67066977+Kingston-5@users.noreply.github.com>
2022-01-18 19:22:09 +0200

    Update DB.php

--- classes/DB.php
+++ classes/DB.php
@@ -49,7 +49,7 @@ class DB {
                     $x++;
                 }
             }
-            var_dump($this->_query);
+            
             if($this->_query->execute()) {
                 $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
                 $this->_count = $this->_query->rowCount();
